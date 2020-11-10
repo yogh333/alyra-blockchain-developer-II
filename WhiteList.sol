7fa632dc4e03d465763e2d1c97a6d02af541b8f1
Jerome Caporossi <jerome.caporossi@gmail.com>
2020-11-10 17:19:39 +0100

    addPerson()

--- WhiteList.sol
+++ WhiteList.sol
@@ -1,10 +1,18 @@
 pragma solidity 0.6.11;
 
 contract WhiteList {
+	
 	mapping(address => bool) whitelist;
 
 	struct Person {
 		string name;
 		uint8 age;
+	}
+
+    Person[] people;
+
+	function addPerson(string memory _name, uint8 _age) public {
+		Person memory person = Person(_name, _age);
+		people.push(person);
 	}
 }
