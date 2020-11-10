c95506b984c4daef3d680b58dfe26b2a238ae587
Jerome Caporossi <jerome.caporossi@gmail.com>
2020-11-10 16:58:18 +0100

    added Person struc

--- WhiteList.sol
+++ WhiteList.sol
@@ -1,5 +1,10 @@
 pragma solidity 0.6.11;
 
 contract WhiteList {
-	mapping (address => bool) whitelist;
+	mapping(address => bool) whitelist;
+
+	struct Person {
+		string name;
+		uint8 age;
+	}
 }
